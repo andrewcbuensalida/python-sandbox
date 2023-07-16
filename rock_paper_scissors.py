e86017cbb1f4d0bf362f53d8f6d124cb8a96160e
Andrew Buensalida <andrewcbuensalida@gmail.com>
2023-07-16 11:49:43 -0700

    rock paper scissors refactored

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,8 +1,9 @@
 import random
 
+
 def get_choices():
     player_choice = input("Rock, paper, or scissors? ")
-    options = ["rock","paper","scissors"]
+    options = ["rock", "paper", "scissors"]
     # random_index = (random.random() * 3).__floor__()
     # computer_choice = options[random_index]
     # OR
@@ -11,17 +12,30 @@ def get_choices():
 
     return choices
 
+
 def check_win(choices):
+    choices["player"] = choices["player"].lower()
     # print("The computer chose " + choices["computer"])
     #  OR
+
     print(f"The computer chose {choices['computer']}")
-    if choices["player"] == "rock" and choices["computer"] == "rock":
+    if choices["player"] == choices["computer"]:
         return "Draw"
-    if choices["player"] == "rock" and choices["computer"] == "paper":
-        return "You lose"
-    if choices["player"] == "rock" and choices["computer"] == "scissors":
-        return "You win"
-    
+    if choices["player"] == "rock":
+        if choices["computer"] == "paper":
+            return "You lose"
+        else:
+            return "You win"
+    if choices["player"] == "paper":
+        if choices["computer"] == "rock":
+            return "You win"
+        else:
+            return "You lose"
+    if choices["player"] == "scissors":
+        if choices["computer"] == "rock":
+            return "You lose"
+        else:
+            return "You win"
 
 
 def play():
