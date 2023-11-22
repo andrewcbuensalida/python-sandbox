e26ede1aacbd3b379130ac67ba8de3ea563b7620
Andrew Buensalida <andrewcbuensalida@gmail.com>
2023-11-22 13:07:15 -0800

    fuzzer with more words

--- fuzzer.py
+++ fuzzer.py
@@ -8,10 +8,19 @@ import sys
 print("start")
 
 # This is <_io.TextIOWrapper name='<stdin>' mode='r' encoding='cp1252'>
-print(sys.stdin)
+# print(sys.stdin)
 
 for word in sys.stdin:
-    print("This is word: " + word)
-# response = requests.get(url="https://swapi.dev/api/people/40")
-
-# print(response.json())
+    print("\n==============This is word: " + word)
+    try:
+        response = requests.get(url=f"https://swapi.dev/api/{word}")
+        print("This is response")
+        print(response)  # <Response [200]>
+        if response.ok:
+            print("This is response.json() ")
+            print(response.json())
+        else:
+            print("In else")
+            raise TypeError("Sorry!!")  # This Sorry!! is useless
+    except TypeError:
+        print("In TypeError")
