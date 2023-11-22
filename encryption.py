f4140fdbdfe24a8b00b3016e1b27b528d32e1106
Andrew Buensalida <andrewcbuensalida@gmail.com>
2023-11-22 15:02:54 -0800

    encryption

---
+++ encryption.py
@@ -0,0 +1,22 @@
+import getpass
+import base64
+
+password = getpass.getpass("Password: ")
+
+print(password)
+
+def encrypt(password: str):
+    encrypted = base64.b64encode(password.encode())
+    print(encrypted)
+    return encrypted
+
+
+def decode(password: bytes):
+    decrypted = base64.b64decode(password)
+    print(decrypted.decode())
+    return decrypted
+
+
+encrypted = encrypt(password)
+
+decrypted = decode(encrypted)
