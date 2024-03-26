68371abe458c38841bccc744d34513fa88eaa4e9
Andrew Buensalida <andrewcbuensalida@gmail.com>
2024-03-26 06:52:10 -0700

    matplotlib, numpy

---
+++ json1.py
@@ -0,0 +1,10 @@
+import json
+
+
+
+
+
+with open('test.json') as f:
+    print(f)
+    j = json.load(f)
+    print(j)
--- sandbox.py
+++
@@ -1,4 +0,0 @@
-client_message = 'hey'
-message = "".join(["Your message is this", client_message]).encode('ascii')
-
-print('*********Example message: ', message)
