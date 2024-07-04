0d45c8a29f2194e18c2be74bc8f557aebd01b8bc
Andrew Buensalida <andrewcbuensalida@gmail.com>
2024-07-04 14:06:01 -0700

    enumerate

--- forloop.py
+++ forloop.py
@@ -1,3 +1,7 @@
 a = ['b','c','d','e','f','g','h']
 for letter in range(len(a)):
     print(a[letter] + ' is at index ' + str(letter) + ' in the list a')
+
+
+for i, l in enumerate(a):
+    print(l + ' is at index ' + str(i) + ' in the list a')
---
+++ stringy.py
@@ -0,0 +1,11 @@
+test = {
+    "red": 'abc' 'lll'
+        ' def'
+        }
+
+print(test)
+
+test2 = 'blue' 'orange'
+'yellow'
+
+print(test2)
