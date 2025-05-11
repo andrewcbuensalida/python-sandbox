aad34fae56e8669ded11b2cc0ae7c78d2cc93fac
Andrew Buensalida <andrewcbuensalida@gmail.com>
2025-05-11 10:12:52 -0700

    dfs

--- web_crawler_as_completed_2.py
+++ web_crawler_as_completed_2.py
@@ -8,12 +8,15 @@
 #        :type url: str
 #        :rtype List[str]
 #        """
-import asyncio
 import time
 from typing import List 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+# news.yahoo.com 1 news.yahoo.com/1 2 news.yahoo.com/100 3                     4 []
+#                  news.yahoo.com/2                        news.yahoo.com/200    []
 
 class HtmlParser:
-    async def getUrls(self, url: str) -> List[str]:
+    def getUrls(self, url: str) -> List[str]:
         time.sleep(1) # Simulate network delay
         if url == "http://news.yahoo.com":
             return ["http://news.yahoo.com", "http://news.google.com","http://news.yahoo.com/1","http://news.yahoo.com/2"]
@@ -28,45 +31,35 @@ class HtmlParser:
         else:
             return []
 
+
 class Solution:
     def get_domain(self,url):
         return url.split('/')[2]
     
-    async def crawl_helper(self, stack, visited, htmlParser, domain):
-        current_url = stack.pop()
-        if current_url not in visited:
-            visited.add(current_url)
-        else:
-            return
-
-        new_urls = await htmlParser.getUrls(current_url)
-        
-        for new_url in new_urls:
-            if new_url not in visited and domain == self.get_domain(new_url):
-                stack.append(new_url)
-
-    async def crawl(self, startUrl: str, htmlParser ) -> List[str]:
-        stack = [startUrl]
-        visited = set([])
-        domain = self.get_domain(startUrl)
-
-        while stack:
-            print("Stack:", stack)
-            # this runs per batch of new urls
-            async with asyncio.TaskGroup() as tg:
-                for i in range(len(stack)):
-                    print(i)
-                    tg.create_task(self.crawl_helper(stack, visited, htmlParser, domain))
-                print("TaskGroup finished")
-
-
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        hostname = self.get_domain(startUrl)
+        tasks = []
+        visited = set()
+        visited.add(startUrl)
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            tasks.append(executor.submit(htmlParser.getUrls, startUrl))
+            while tasks:
+                new_tasks = []
+                for future in as_completed(tasks):
+                    urls = future.result()
+                    for url in urls:
+                        if url not in visited and self.get_domain(url) == hostname:
+                            visited.add(url)
+                            new_tasks.append(executor.submit(htmlParser.getUrls, url))
+                tasks = new_tasks
         return list(visited)
+
     
 crawler = Solution()
 startUrl = "http://news.yahoo.com"
 parser = HtmlParser()
 start_time = time.perf_counter()
-result = asyncio.run(crawler.crawl(startUrl, parser))
+result = crawler.crawl(startUrl, parser)
 end_time = time.perf_counter()
-print(f"Execution time: {end_time - start_time} seconds") # should be around 3 seconds if using asyncio task. 5 seconds if not using asyncio task.
+print(f"Execution time: {end_time - start_time} seconds") # should be around 4 seconds with multithreading. 7 seconds if not.
 print(result)
---
+++ web_crawler_dfs_ThreadPoolEx.py
@@ -0,0 +1,61 @@
+# """
+# This is HtmlParser's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class HtmlParser(object):
+#    def getUrls(self, url):
+#        """
+#        :type url: str
+#        :rtype List[str]
+#        """
+import time
+from typing import List 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from threading import Lock
+
+# news.yahoo.com 1 news.yahoo.com/1 2 news.yahoo.com/100 3                     4 []
+#                  news.yahoo.com/2                        news.yahoo.com/200    []
+
+class HtmlParser:
+    def getUrls(self, url: str) -> List[str]:
+        time.sleep(1) # Simulate network delay
+        if url == "http://news.yahoo.com":
+            return ["http://news.yahoo.com", "http://news.google.com","http://news.yahoo.com/1","http://news.yahoo.com/2"]
+        elif url == "http://news.yahoo.com/1":
+            return ["http://news.yahoo.com", "http://news.google.com","http://news.yahoo.com/100"]
+        elif url == "http://news.yahoo.com/100":
+            time.sleep(1) # Simulate network delay
+            return []
+        elif url == "http://news.yahoo.com/2":
+            time.sleep(1) # Simulate network delay
+            return ["http://news.yahoo.com", "http://news.google.com","http://news.yahoo.com/200"]
+        else:
+            return []
+
+
+class Solution:
+    lock = Lock()
+    def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
+        visited = set()
+        initialHostname = startUrl.split("/")[2]
+
+
+        def _dfs(url):
+            with self.lock:
+                visited.add(url)
+            with ThreadPoolExecutor(16) as executor:
+                for nextUrl in htmlParser.getUrls(url):
+                    if nextUrl.split("/")[2] == initialHostname and nextUrl not in visited:
+                        executor.submit(_dfs, nextUrl)
+        
+        _dfs(startUrl)
+        return list(visited)
+    
+crawler = Solution()
+startUrl = "http://news.yahoo.com"
+parser = HtmlParser()
+start_time = time.perf_counter()
+result = crawler.crawl(startUrl, parser)
+end_time = time.perf_counter()
+print(f"Execution time: {end_time - start_time} seconds") # should be around 3 seconds with multithreading. 5 seconds if not using asyncio task.
+print(result)
