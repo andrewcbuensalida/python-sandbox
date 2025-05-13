ae82748997570d8ff3eda3b057ca62efc35d4939
Andrew Buensalida <andrewcbuensalida@gmail.com>
2025-05-12 22:20:02 -0700

    Add initial implementations for list multiplication and dictionary sorting

---
+++ multiply_list.py
@@ -0,0 +1,20 @@
+x = [1,2,'a'] * 3
+print(x)
+
+# x= [1,2] + 100
+# print(x) error
+
+x = [1,2] +[3,4]
+print(x)
+x = [1,20] < [3,0]
+print(x)
+
+
+x = {
+    'name':'drew',
+    'age':37
+}
+
+y = x.keys()
+
+print(type(y))
---
+++ sort_dict.py
@@ -0,0 +1,21 @@
+ages = {
+    "age1":'100',
+    "age2":'10',
+    "age3":'b',
+    "age4":'1',
+    "age5":'a',
+}
+
+ages_list = list(ages.items())
+print(ages_list)
+ages_list.sort(key=lambda age:age[1])
+print(ages_list)
+new_dict = dict(ages_list)
+print(new_dict)
+
+
+print('a'<'b')
+# print('a'<1) error
+
+print(sorted(new_dict.items(),key=lambda age:age[0],reverse=True))
+print(new_dict)
